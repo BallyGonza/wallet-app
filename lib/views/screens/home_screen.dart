7bfe60fd733409cc292f86efdf46ef14ed8b0852
Gonzalo Bally <gonzalo.bally@urbetrack.com>
2023-03-30 16:36:52 -0300

    Update home_screen.dart

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,13 +1,181 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    return Scaffold(
+      backgroundColor: const Color(0xff1E1E1E),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 16.0),
+              child: Text(
+                'Accounts balance',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Card(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Balance',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                IncomeCard(),
+                ExpenseCard(),
+              ],
+            ),
+            const SizedBox(height: 16),
+            const Card(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Transactions',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return const Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        'Transactions',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class IncomeCard extends StatelessWidget {
+  const IncomeCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(right: 4.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.green,
+              radius: 10,
+              child: FaIcon(
+                FontAwesomeIcons.arrowUp,
+                size: 12,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Column(
+            children: const [
+              Text(
+                'Icomes',
+                style: TextStyle(
+                  fontSize: 9,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                '\$ 1,000.00',
+                style: TextStyle(
+                  fontSize: 9,
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ExpenseCard extends StatelessWidget {
+  const ExpenseCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(right: 4.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.red,
+              radius: 10,
+              child: FaIcon(
+                FontAwesomeIcons.arrowDown,
+                size: 12,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: const [
+              Text(
+                'Expenses',
+                style: TextStyle(
+                  fontSize: 8,
+                  color: Colors.grey,
+                ),
+              ),
+              Text(
+                '\$ 1,000.00',
+                style: TextStyle(
+                  fontSize: 9,
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
