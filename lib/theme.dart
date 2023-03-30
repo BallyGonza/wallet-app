7e24c1a6c72acb5e573cde66e33b9558ee4e0ff9
Gonzalo Bally <gonzalo.bally@urbetrack.com>
2023-03-30 15:18:18 -0300

    Working

---
+++ lib/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme = ThemeData(
+  primaryColor: Colors.white,
+  scaffoldBackgroundColor: Colors.white,
+  textTheme: const TextTheme(
+    headline1: TextStyle(
+      fontSize: 72.0,
+      fontWeight: FontWeight.bold,
+    ),
+    headline6: TextStyle(
+      fontSize: 36.0,
+      fontStyle: FontStyle.italic,
+    ),
+    bodyText2: TextStyle(
+      fontSize: 14.0,
+      fontFamily: 'Hind',
+    ),
+  ),
+);
